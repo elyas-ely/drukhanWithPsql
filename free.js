2f4d136448da693a82b7eb53e365caadf91db82e
ELyas <97180899+elyas-ely@users.noreply.github.com>
2025-04-07 15:33:15 +0430

    free file to comment

--- free.js
+++ free.js
@@ -1,26 +1,26 @@
-import pkg from 'pg'
-const { Client } = pkg
-import dotenv from 'dotenv'
-import { logger } from '../utils/logger.js'
+// import pkg from 'pg'
+// const { Client } = pkg
+// import dotenv from 'dotenv'
+// import { logger } from '../utils/logger.js'
 
-dotenv.config()
+// dotenv.config()
 
-const client = new Client({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-})
+// const client = new Client({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// })
 
-const connectDb = async () => {
-  try {
-    await client.connect()
-    logger.info('Connected to the database')
-  } catch (err) {
-    console.error('Failed to connect to the database:', err)
-    process.exit(1)
-  }
-}
+// const connectDb = async () => {
+//   try {
+//     await client.connect()
+//     logger.info('Connected to the database')
+//   } catch (err) {
+//     console.error('Failed to connect to the database:', err)
+//     process.exit(1)
+//   }
+// }
 
-export { client, connectDb }
+// export { client, connectDb }
