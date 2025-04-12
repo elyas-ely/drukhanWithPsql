cf4c088776c5e4b9f63a80ac8df3a1fca6605ecd
ELyas <97180899+elyas-ely@users.noreply.github.com>
2025-04-12 13:49:49 +0430

    create post updated

--- src/services/postService.js
+++ src/services/postService.js
@@ -226,7 +226,7 @@ const createPostFn = async (postData) => {
         user_id 
       )
       VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
-      RETURNING *,`
+      RETURNING *`
 
     const postValues = [
       car_name,
