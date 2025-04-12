4e1ec9d93b32ed66d9f91d3096ddafa2b4c69c8f
ELyas <97180899+elyas-ely@users.noreply.github.com>
2025-04-12 14:27:23 +0430

    updated to pool
    everything works fine

--- src/config/db.js
+++ src/config/db.js
@@ -9,7 +9,7 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   max: 20, // Maximum number of clients in the pool
   idleTimeoutMillis: 30000, // How long a client is allowed to remain idle before being closed
-  connectionTimeoutMillis: 2000, // How long to wait for a connection
+  connectionTimeoutMillis: 5000, // How long to wait for a connection
 })
 
 // Add event listeners for pool errors
