58dc4e0f9a5158712673d82ad2be5a29c41636f3
ELyas <97180899+elyas-ely@users.noreply.github.com>
2025-08-13 12:37:15 +0430

    n

--- src/config/db.js
+++ src/config/db.js
@@ -10,7 +10,7 @@ const pool = new Pool({
   max: 10, // Maximum number of clients in the pool
   min: 2,
   idleTimeoutMillis: 10000, // How long a client is allowed to remain idle before being closed
-  connectionTimeoutMillis: 5000, // How long to wait for a connection
+  connectionTimeoutMillis: 5000, // How long to wait for a connection .
 })
 
 // Add event listeners for pool errors
